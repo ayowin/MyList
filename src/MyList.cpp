192d5f96b210b79b33436a8be948885502bd54dc
OuyangWei <18344175265@126.com>
2018-03-12 13:43:33 +0800

    OuyangWei

---
+++ src/MyList.cpp
@@ -0,0 +1,130 @@
+
+#include "MyList.h"
+
+struct Node
+{
+	int data = 0;
+	Node* next = NULL;
+};
+
+MyList::MyList()
+{
+	head = new Node;
+}
+
+MyList::~MyList()
+{
+	if (head)
+	{
+		Node *p = head;
+		while (p->next)
+		{
+			head = p->next;
+			delete p;
+			p = head;
+		}
+		delete head;
+		head = NULL;
+	}
+}
+
+bool MyList::insert(int position, int data)
+{
+	Node* p = head;
+	if (position <= size() + 1 && position >0)
+	{
+		for (int i = 1; i < position; i++)
+		{
+			p = p->next;
+		}
+		Node* temp = new Node;
+		temp->data = data;
+		if (p->next)
+		{
+			temp->next = p->next;
+		}
+		p->next = temp;
+		(head->data)++;
+	}
+	else return false;
+	return true;
+}
+
+bool MyList::isExist(int position)
+{
+	if (position > 0 && position <= size())return true;
+	else return false;
+}
+
+int MyList::get(int position)
+{
+	if (isExist(position))
+	{
+		Node* p = head;
+		for (int i = 0; i < position;i++)
+		{
+			p = p->next;
+		}
+		return p->data;
+	}
+	return false;
+}
+
+bool MyList::update(int position, int data)
+{
+	if (isExist(position))
+	{
+		Node* p = head;
+		for (int i = 0; i < position; i++)
+		{
+			p = p->next;
+		}
+		p->data = data;
+		return true;
+	}
+	return false;
+}
+
+bool MyList::remove(int position)
+{
+	if (isExist(position))
+	{
+		Node* p = head;
+		for (int i = 1; i < position; i++)
+		{
+			p = p->next;
+		}
+		if (p->next->next)
+		{
+			Node* temp = p->next->next;
+			delete p->next;
+			p->next = temp;
+		}
+		else
+		{
+			delete p->next;
+			p->next = NULL;
+		}
+		(head->data)--;
+		return true;
+	}
+	return false;
+}
+
+int MyList::size()
+{
+	return head->data;
+}
+
+void MyList::print()
+{
+	Node* p = head;
+	std::cout << "总共有" << head->data << "个数据。" << std::endl;
+	std::cout << "链表中的数据:";
+	for (int i = 0; i < head->data; i++)
+	{
+		p = p->next;
+		std::cout << p->data << " ";
+	}
+	std::cout << std::endl;
+}
---
+++ src/MyList.h
@@ -0,0 +1,34 @@
+
+#ifndef _MyList_H
+#define _MyList_H
+
+#include <iostream>
+
+struct Node;
+
+class MyList
+{
+public:
+	MyList();
+	~MyList();
+
+	bool insert(int position,int data);//插入数据
+
+	bool isExist(int position);//该位置是否有值
+	
+	int get(int position);//取得数据
+
+	bool update(int position, int data);//更新数据
+
+	bool remove(int position);//删除数据
+
+	int size();//取得链表长度
+
+	void print();//调试输出
+
+private:
+	Node* head;
+};
+
+#endif
+
